0bf8070d1fa6e0cc4cddeeea8da4423b0581e202
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-10-14 16:35:01 +0530

    local run changes

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-docker-compose up --build -d --remove-orphans --no-deps
+# For Docker
+# docker-compose up --build -d --remove-orphans --no-deps
+# Locally (without VirtualEnv)
+python3 -m streamlit run src/streamlit_app.py
+# Locally (with VirtualEnv)
+# streamlit run src/streamlit_app.py
+
 echo "Finished"
